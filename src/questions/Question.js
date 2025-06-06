7c625b78244ff123e3baff9d4698bf1d5e256c0c
Jessica Organ <jorgan612@gmail.com>
2025-06-05 20:07:31 -0600

    Removed hidden css class. Cleaned up code lines and fixed issue with unanswered styling rendered with erorr text addition.

--- src/questions/Question.js
+++ src/questions/Question.js
@@ -5,19 +5,19 @@ function Question({questions, completeQuiz, quizComplete, setSelectedAnswer, sco
 
     return (
         <div className='question'>
-                {questions.map((q) => (
-            <div className={`${(q.selectedAnswer === null && quizComplete) && 'unanswered'}`} key={q.question}>
-                <p>{q.question}</p>
-                <Answer question={q} quizComplete={quizComplete} setSelectedAnswer={setSelectedAnswer} />
-                <div>******************************************************************************************</div>
-            </div>
-                ))}
-                {<button onClick={completeQuiz}>Finish Quiz</button>}
+            {questions.map((q) => (
+                <div className={`${(q.selectedAnswer === null && quizComplete) && 'unanswered'}`} key={q.question}>
+                    <p>{q.question}</p>
+                    <Answer question={q} quizComplete={quizComplete} setSelectedAnswer={setSelectedAnswer} />
+                    <div>******************************************************************************************</div>
+                </div>
+            ))}
 
-                {score?.unanswered.length > 0 && <p className='error-text'> Oh no! One or more questions are missing a selected answer. Please go back and answer all questions then click Finish Quiz.</p>}
+            {<button onClick={completeQuiz}>Finish Quiz</button>}
+
+            {(score?.unanswered.length > 0) && <p className='error-text'> Oh no! One or more questions are missing a selected answer. Please go back and answer all questions then click Finish Quiz.</p>}
         </div>
     )
 }
-
 
 export default Question;
